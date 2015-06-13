0988be2f15697df6d1c0a476371325ea0b7c30d8
imontalvomiguel <ubaldo.im@gmail.com>
2015-06-13 00:45:34 -0500

    Ahora si guarda el password

--- app/HireMe/Entities/User.php
+++ app/HireMe/Entities/User.php
@@ -20,7 +20,7 @@ class User extends \Eloquent implements UserInterface, RemindableInterface {
    */
   public function setPasswordAttribute($value) {
 
-    if (!empty($velue))
+    if (!empty($value))
     {
       $this->attributes['password'] = \Hash::make($value);
     }
